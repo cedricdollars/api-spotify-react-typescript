4000e07d53c6736d95442e5cc6746531fb909627
ngoune_cedric <gabyngoune@yahoo.fr>
2020-08-31 22:58:22 +0200

    définir la page de connexion de l'api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.scss';
 import AppComponent from './components/App/app';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Navbar from './Navbar/navbar';
 import './app.scss';
 
 const App: React.FC = () => {
-    return <div className="container-app">Api Spotify</div>;
+    return (
+        <div className="container-app">
+            <div className="container">
+                <Navbar />
+                <div className="upperNav">
+                    <div className="signup">Sign up</div>
+                    <div className="login">Login</div>
+                </div>
+            </div>
+            <div className="music-control">music controll</div>
+        </div>
+    );
 };
 export default App;
